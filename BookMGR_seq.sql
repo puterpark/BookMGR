efcb0d2f57fa94d1e3f531a911531e646e8bc52f
Puter <36284842+puterpark@users.noreply.github.com>
2018-05-01 17:26:37 +0900

    SQL

---
+++ BookMGR_seq.sql
@@ -0,0 +1,13 @@
+--------------------------------------------------------
+--  파일이 생성됨 - 화요일-5월-01-2018   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Sequence BOOK_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "CUSTOM"."BOOK_SEQ"  MINVALUE 1 MAXVALUE 999999999999999999999999999 INCREMENT BY 1 START WITH 41 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence MEM_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "CUSTOM"."MEM_SEQ"  MINVALUE 1 MAXVALUE 999999999999999999999999999 INCREMENT BY 1 START WITH 61 CACHE 20 NOORDER  NOCYCLE ;
